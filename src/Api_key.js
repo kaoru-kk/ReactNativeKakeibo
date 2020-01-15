214f25b1f932eb299fd184962e82b38d552e64bd
kaoru_k <49483118+19980410@users.noreply.github.com>
2020-01-15 15:02:32 +0900

    Update Api_key.js

--- src/Api_key.js
+++ src/Api_key.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyCZzW8ErWsgysg7sorvmLlO8EwyW3T8DUk",
-    authDomain: "kakeibo-a0ef8.firebaseapp.com",
-    databaseURL: "https://kakeibo-a0ef8.firebaseio.com",
-    projectId: "kakeibo-a0ef8",
-    storageBucket: "kakeibo-a0ef8.appspot.com",
-    messagingSenderId: "661128524394",
-    appId: "1:661128524394:web:858835325c97ec340d5013",
-    measurementId: "G-VK7QLYLT6N"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 let firebaseApp =  firebase.initializeApp(config);
